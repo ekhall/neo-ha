07ed1c0cfd1949b6373032611443c5fc6c3ec9ca
E. Kevin Hall <hall@absinthe.org>
2013-10-07 22:34:46 -0400

    Origin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JrubyTen::Application.config.secret_token = 'e633884ce448d62f940295ecf6331813161bb5cb5a14fded3300b56280abad5bc9510b08eb54fd1fc56d14b3f76f940dcb880ddcaaca0962bafe96c9c3b64130'
